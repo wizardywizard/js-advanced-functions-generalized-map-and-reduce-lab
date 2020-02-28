6560cd600bc94b430be7477c1f85f1dc39341aa1
wizardywizard <littledavid175@gmail.com>
2020-02-27 21:39:13 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(array, func){
+  let answer = []
+  let i = 0
+  while(i < array.length){
+    answer.push(func(array[i]))
+    i += 1
+  }
+  return answer
+}
+
+function reduce(array, sv=0){
+    if (sv){
+        let sum =sv
+        let i =0
+    }else{
+        let sum = array[0]
+        let i = 1
+    }
+    while (i < array.length){
+        console.log(sum)
+        i += 1
+    }
+    return sum
+}
